b0414c681aa9e8241fc09676247fe39aaca8eb6d
Maruf Hassan <mrfhassan17@gmail.com>
2020-10-29 20:02:30 +0530

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,94 @@
+import 'package:covid_api_app/app/services/api_service.dart';
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
- 
+
+import 'app/services/api.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _accessToken = '';
+  int _cases;
+  int _deaths;
+
+  void _updateAccessToken() async {
+    final apiService = APIService(API.sandbox());
+    final accessToken = await apiService.getAccessToken();
+    final cases = await apiService.getEndpointData(
+      accessToken: accessToken,
+      endpoint: Endpoint.cases,
+    );
+    final deaths = await apiService.getEndpointData(
+      accessToken: accessToken,
+      endpoint: Endpoint.deaths,
+    );
+
+    setState(() {
+      _accessToken = accessToken;
+      _cases = cases;
+      _deaths = deaths;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              'API - $_accessToken',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            if (_cases != null)
+              Text(
+                'Cases - $_cases',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            if (_deaths != null)
+              Text(
+                'Deaths - $_deaths',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+          ],
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _updateAccessToken,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
